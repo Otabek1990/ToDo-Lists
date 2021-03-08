3adc24bb54ff7dd3a8af8df8413586dcc56d7cbf
Your Name <you@example.com>
2021-03-08 14:56:36 +0500

    Updates

---
+++ precache-manifest.bc9f66a26a163c33c120804d3f9a2a34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df7fe5e7d23b9e46b79d194679bb53cd",
+    "url": "/ToDo-Lists/index.html"
+  },
+  {
+    "revision": "4b2ef3ff5e6edacd7e3b",
+    "url": "/ToDo-Lists/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "a55e0bb3ed9efb595bed",
+    "url": "/ToDo-Lists/static/css/main.beca6410.chunk.css"
+  },
+  {
+    "revision": "4b2ef3ff5e6edacd7e3b",
+    "url": "/ToDo-Lists/static/js/2.f74aa4fc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ToDo-Lists/static/js/2.f74aa4fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a55e0bb3ed9efb595bed",
+    "url": "/ToDo-Lists/static/js/main.18fdbe7f.chunk.js"
+  },
+  {
+    "revision": "609b7c3087927a8d52c5",
+    "url": "/ToDo-Lists/static/js/runtime-main.430bb144.js"
+  }
+]);
